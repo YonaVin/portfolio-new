714c50f7c87d4a1e9af544eb5aac506c73baa42d
YonaVin7 <yona.vin@thewideconnect.com>
2025-08-26 09:10:57 +0900

    new

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './App.css';
 
-// Projects data
+// Projects datagggggg
 const projects = [
   {
     title: "E-commerce Platform",
